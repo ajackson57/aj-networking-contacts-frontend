bf498aa587fb24d7840cd36f0a394964e790d881
ajackson57 <andrewm.jackson@libertymutual.com>
2017-10-19 14:14:09 -0400

    Initial commit for front-end of project

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://gentle-crag-17128.herokuapp.com'
+    development: 'http://localhost:4741'
+  }
+}
+
+module.exports = config
