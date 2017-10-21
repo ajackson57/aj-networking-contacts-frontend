c64179e18c899afabdb564b81759e13885418325
ajackson57 <andrewm.jackson@libertymutual.com>
2017-10-21 12:08:37 -0400

    Update pattern for events and cleanup edit contact
    Need to now add authentication to the contact controller and api calls

--- assets/scripts/contacts/events-contacts.js
+++ assets/scripts/contacts/events-contacts.js
@@ -2,12 +2,21 @@
 
 const api = require('./api-contacts.js')
 const ui = require('./ui-contacts.js')
-// const store = require('../store')
+const store = require('../store')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const getContactsSuccess = (data) => {
+  console.log(data)
+  store.contacts = data.contacts
+  ui.displayContacts(store.contacts)
+  $('.edit-contact').on('click', editContact)
+}
 
 const onGetContacts = (event) => {
   event.preventDefault()
+  ui.clearContacts()
   api.getContacts()
-    .then(ui.getContactsSuccess)
+    .then(getContactsSuccess)
     .catch(ui.failure)
 }
 
@@ -16,25 +25,40 @@ const onClearContacts = (event) => {
   ui.clearContacts()
 }
 
-// const onSaveContact = (event) => {
-//   console.log('Saved')
-//   ui.clearContacts()
-//   ui.displayContacts(store.contacts)
-// }
-//
-// const onCancelContact = (event) => {
-//   console.log('Cancelled')
-//   ui.clearContacts()
-//   ui.displayContacts(store.contacts)
-// }
+const onSaveContact = (event) => {
+  console.log('Saved')
+  event.preventDefault()
+  const contactId = store.contactId
+  const contactData = getFormFields(event.target.form)
+  api.updateContact(contactId, contactData)
+    .then(ui.updateContactSuccess)
+    .catch(ui.failure)
+}
+
+const onCancelContact = (event) => {
+  console.log('Cancelled')
+  ui.clearContacts()
+  ui.displayContacts(store.contacts)
+}
 
 const addHandlers = () => {
   $('#getContactsButton').on('click', onGetContacts)
   $('#clearContactsButton').on('click', onClearContacts)
 }
 
+const editContact = (event) => {
+  const contactId = event.currentTarget.attributes.getNamedItem('row-id').value
+  const contact = store.contacts[contactId - 1]
+  store.contactId = contactId
+  ui.clearContacts()
+  ui.displayContact(contact)
+  $('button#save-contact').on('click', onSaveContact)
+  $('button#cancel-contact').on('click', onCancelContact)
+}
+
 module.exports = {
-  addHandlers
-  // onSaveContact,
-  // onCancelContact
+  addHandlers,
+  editContact,
+  onSaveContact,
+  onCancelContact
 }
--- assets/scripts/contacts/ui-contacts.js
+++ assets/scripts/contacts/ui-contacts.js
@@ -1,27 +1,24 @@
 'use strict'
-const api = require('./api-contacts.js')
+// const api = require('./api-contacts.js')
 const store = require('../store')
 const showContactsTemplate = require('../templates/contact-listing.handlebars')
 const showContactEditTemplate = require('../templates/contact-edit.handlebars')
-const eve = require('./events-contacts.js')
+// const eve = require('./events-contacts.js')
 
 const displayContacts = (contacts) => {
   console.log(contacts)
   store.contacts = contacts
   const showContactsHtml = showContactsTemplate({ contacts: contacts })
   $('.content').append(showContactsHtml)
-  $('.edit-contact').on('click', editContact)
 }
 
-const getContactsSuccess = (data) => {
-  console.log(data)
-  store.contacts = data.contacts
-  displayContacts(store.contacts)
+const displayContact = (contact) => {
+  const showContactEditHtml = showContactEditTemplate({ contact: contact })
+  $('.content').append(showContactEditHtml)
 }
 
 const updateContactSuccess = (data) => {
-  clearContacts()
-  displayContacts(store.contacts)
+  $('#getContactsButton').trigger('click')
 }
 
 const clearContacts = () => {
@@ -32,36 +29,10 @@ const failure = (error) => {
   console.error(error)
 }
 
-const onSaveContact = (event) => {
-  console.log('Saved')
-  event.preventDefault()
-  const contactId = store.contactId
-  const contact = store.contacts[contactId - 1]
-  api.updateContact(contactId, contact)
-    .then(updateContactSuccess)
-    .catch(failure)
-}
-
-const onCancelContact = (event) => {
-  console.log('Cancelled')
-  clearContacts()
-  displayContacts(store.contacts)
-}
-
-const editContact = (event) => {
-  const contactId = event.currentTarget.attributes.getNamedItem('row-id').value
-  const contact = store.contacts[contactId - 1]
-  store.contactId = contactId
-  clearContacts()
-  const showContactEditHtml = showContactEditTemplate({ contact: contact })
-  $('.content').append(showContactEditHtml)
-  $('button#save-contact').on('click', onSaveContact)
-  $('button#cancel-contact').on('click', onCancelContact)
-}
-
 module.exports = {
-  getContactsSuccess,
   clearContacts,
   displayContacts,
-  failure
+  displayContact,
+  failure,
+  updateContactSuccess
 }
