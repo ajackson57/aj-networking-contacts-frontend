60cf5d8909440badcc91ced4b69141c6b2c1c035
ajackson57 <andrewm.jackson@libertymutual.com>
2017-10-23 10:27:04 -0400

    Add infrastructure files for context menu
    Add bootstrap files or context menu

--- assets/scripts/contacts/events-contacts.js
+++ assets/scripts/contacts/events-contacts.js
@@ -4,6 +4,11 @@ const api = require('./api-contacts.js')
 const ui = require('./ui-contacts.js')
 const store = require('../store')
 const getFormFields = require('../../../lib/get-form-fields')
+// const BootstrapMenu = require('bootstrap-menu');
+
+// var menu = new BootstrapMenu('#dropdownButton', {
+//   actions: /* ... */
+// });
 
 const exampleContact = { first_name: 'Wylie',
   last_name: 'Coyote',
@@ -16,7 +21,44 @@ const getContactsSuccess = (data) => {
   store.contacts = data.contacts
   ui.displayContacts(store.contacts)
   $('.edit-contact').on('click', editContact)
+  // console.log($('#grid'))
+//   const menu = new BootstrapMenu('#grid', {
+//   /* a function to know which row was the context menu opened on,
+//    * given the selected DOM element. When this function is defined,
+//    * every user-defined action callback receives its return value as
+//    * an argument. */
+//     fetchElementData: function($rowElem) {
+//       const rowId = $rowElem.data('rowId')
+//     // return demo3Rows[rowId]
+//   },
+//   actions: [{
+//     name: 'Edit name',
+//     onClick: function(row) {
+//       toastr.info("'Edit name' clicked on '" + row.name + "'");
+//     }
+//   }, {
+//     name: 'Edit description',
+//     onClick: function(row) {
+//       toastr.info("'Edit description' clicked on '" + row.name + "'");
+//     }
+//   }]
+// });
 }
+
+// $('#grid').bootstrapTable({
+//   contextMenu: '#example1-context-menu',
+//   onContextMenuItem: function (row, $el) {
+//     if ($el.data('item') === 'edit') {
+//       alert('Edit: ' + row.itemid + ' ' + row.name + ' ' + row.price)
+//     } else if ($el.data('item') === 'delete') {
+//       alert('Delete: ' + row.itemid + ' ' + row.name + ' ' + row.price)
+//     } else if ($el.data('item') === 'action1') {
+//       alert('Action1: ' + row.itemid + ' ' + row.name + ' ' + row.price)
+//     } else if ($el.data('item') === 'action2') {
+//       alert('Action2: ' + row.itemid + ' ' + row.name + ' ' + row.price)
+//     }
+//   }
+// })
 
 const onGetContacts = (event) => {
   event.preventDefault()
