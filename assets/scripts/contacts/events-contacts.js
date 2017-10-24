def98be00d1e919a07ada348480823a6e63d0749
ajackson57 <andrewm.jackson@libertymutual.com>
2017-10-24 08:41:38 -0400

    Add context menu and clear out bootstrap files
    Add context menu to provide commands and clean out bootstrap files that
    are not needed. Next need to hook up context menu options.

--- assets/scripts/contacts/events-contacts.js
+++ assets/scripts/contacts/events-contacts.js
@@ -4,61 +4,40 @@ const api = require('./api-contacts.js')
 const ui = require('./ui-contacts.js')
 const store = require('../store')
 const getFormFields = require('../../../lib/get-form-fields')
-// const BootstrapMenu = require('bootstrap-menu');
+// const BootstrapMenu = require('bootstrap-menu')
 
-// var menu = new BootstrapMenu('#dropdownButton', {
-//   actions: /* ... */
-// });
-
-const exampleContact = { first_name: 'Wylie',
+const exampleContact = {
+  first_name: 'Wylie',
   last_name: 'Coyote',
   email: 'will@gail.com',
   company: 'Acme',
-  position: 'Bird Chaser'}
+  position: 'Bird Chaser'
+}
+
+const contextMenu = (event) => {
+  event.preventDefault()
+  $('.context')
+    .show()
+    .css({
+      top: event.pageY,
+      left: event.pageX
+    })
+}
+
+const contextMenuResponse = (event) => {
+  if ($('.context').is(':hover') === false) {
+    $('.context').fadeOut('fast')
+  }
+}
 
 const getContactsSuccess = (data) => {
-  console.log(data)
+  // console.log(data)
   store.contacts = data.contacts
   ui.displayContacts(store.contacts)
   $('.edit-contact').on('click', editContact)
-  // console.log($('#grid'))
-//   const menu = new BootstrapMenu('#grid', {
-//   /* a function to know which row was the context menu opened on,
-//    * given the selected DOM element. When this function is defined,
-//    * every user-defined action callback receives its return value as
-//    * an argument. */
-//     fetchElementData: function($rowElem) {
-//       const rowId = $rowElem.data('rowId')
-//     // return demo3Rows[rowId]
-//   },
-//   actions: [{
-//     name: 'Edit name',
-//     onClick: function(row) {
-//       toastr.info("'Edit name' clicked on '" + row.name + "'");
-//     }
-//   }, {
-//     name: 'Edit description',
-//     onClick: function(row) {
-//       toastr.info("'Edit description' clicked on '" + row.name + "'");
-//     }
-//   }]
-// });
+  $('#context').on('contextmenu', contextMenu)
+  $('.context').click(contextMenuResponse)
 }
-
-// $('#grid').bootstrapTable({
-//   contextMenu: '#example1-context-menu',
-//   onContextMenuItem: function (row, $el) {
-//     if ($el.data('item') === 'edit') {
-//       alert('Edit: ' + row.itemid + ' ' + row.name + ' ' + row.price)
-//     } else if ($el.data('item') === 'delete') {
-//       alert('Delete: ' + row.itemid + ' ' + row.name + ' ' + row.price)
-//     } else if ($el.data('item') === 'action1') {
-//       alert('Action1: ' + row.itemid + ' ' + row.name + ' ' + row.price)
-//     } else if ($el.data('item') === 'action2') {
-//       alert('Action2: ' + row.itemid + ' ' + row.name + ' ' + row.price)
-//     }
-//   }
-// })
 
 const onGetContacts = (event) => {
   event.preventDefault()
@@ -91,7 +70,7 @@ function findContact (contact) {
 }
 
 const onSaveNewContact = (event) => {
-  console.log('Saved')
+  // console.log('Saved')
   event.preventDefault()
   const contactData = getFormFields(event.target.form)
   contactData.contact['user_id'] = store.user.id
@@ -100,7 +79,7 @@ const onSaveNewContact = (event) => {
     .catch(ui.failure)
 }
 const onSaveContact = (event) => {
-  console.log('Saved')
+  // console.log('Saved')
   event.preventDefault()
   const contactId = store.contactId
   const contactData = getFormFields(event.target.form)
@@ -110,7 +89,7 @@ const onSaveContact = (event) => {
 }
 
 const onCancelContact = (event) => {
-  console.log('Cancelled')
+  // console.log('Cancelled')
   event.preventDefault()
   ui.clearContacts()
   ui.displayContacts(store.contacts)
@@ -118,7 +97,7 @@ const onCancelContact = (event) => {
 }
 
 const onDeleteContact = (event) => {
-  console.log('Deleted')
+  // console.log('Deleted')
   event.preventDefault()
   const contactId = store.contactId
   api.deleteContact(contactId)
@@ -133,7 +112,7 @@ const addHandlers = () => {
 }
 
 const editContact = (event) => {
-  const contactId = event.currentTarget.attributes.getNamedItem('row-id').value
+  const contactId = event.currentTarget.parentElement.attributes.getNamedItem('row-id').value
   store.contactId = contactId
   const contact = store.contacts.find(findContact)
   ui.clearContacts()
